4680c916245467e53e158816604dc798320f288d
AndrewPPinner <84006152+AndrewPPinner@users.noreply.github.com>
2021-10-18 00:03:49 -0400

    added comments for my sanity later
    Finish project for now

--- static/app.js
+++ static/app.js
@@ -11,10 +11,13 @@ const app = Vue.createApp({
         }
     },
     methods: {
+        //call function passing the value of card input from front end
         search(card) {
             axios
+            //post call to send value of card to server so it can make correct crawler
             .post("https://gpu-scraper.herokuapp.com/stock", card)
             .then(response => {
+                //return data from server crawler if something is returned set show to true to show cards on front end
                 this.resData = response.data
                 this.show = true
                 console.log(response)
