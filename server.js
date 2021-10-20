8a21c0cb642d02c74b19dd287748302033729700
AndrewPPinner <84006152+AndrewPPinner@users.noreply.github.com>
2021-10-19 20:18:39 -0400

    Bug fix and new feature
    Added the ability to select store you want to search.
    Fixed "NOT CARRIED" from showing up available.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const https = require('https')
 //set to allow heroku to set its own port value
 const port = process.env.PORT || 3000
 const app = express()
-const data = ''
+
 //wake up function to keep heroku app awake
 setInterval(function() {
     https.get("https://gpu-scraper.herokuapp.com/");
@@ -19,13 +19,11 @@ app.use(express.urlencoded());
 //default landing page to serve static HTML and JS
 app.use('/', express.static('static'))
 //to allow post request from front end at /stock url the request is the "card" value from front end send is value sent back to front end via axios request
-app.post('/stock', (req, res) =>{ 
-    //bodyParse comes as OBJ, set it to string to call crawler URl and manipulate it
-    const cardObj = req.body
-    const card = JSON.stringify(cardObj)
+app.post('/stock', (req, res) =>{
+    const url = "https://www.microcenter.com/search/search_results.aspx?Ntt=" + req.body[0].card + "&Ntk=all&sortby=pricelow&N=4294966937&storeid=" + req.body[0].selectLocation
     //axios call to get standard HTML from webpage to be crawled
-axios
-.get("https://www.microcenter.com/search/search_results.aspx?Ntt=" + card + "&Ntk=all&sortby=pricelow&N=4294966937&storeid=051")
+    axios
+    .get(url)
     .then(response => {
         // const to use throughout entire call
         var index = 0
@@ -52,7 +50,7 @@ axios
             //setting the base value of available so that it can be set as true or false
             var available = null
             //if statment to check if returned data frm "stock" is equal to SOLD OUT if so set value to false for front end to set text color to red
-            if(stock.startsWith('SOLD OUT')) {
+            if(stock.startsWith('SOLD OUT') || stock.startsWith("NOT")) {
                 available = false
             }
             else {
--- static/app.js
+++ static/app.js
@@ -6,16 +6,71 @@ const app = Vue.createApp({
             price: "",
             stock: "",
             link: "",
+            selectLocation: "",
             show: false,
-            resData: [{}]
+            sentReq: [],
+            resData: [{}],
+            locationArray:[
+                {location: "Duluth, GA",
+                id: "065"},
+                {location: "Tustin, CA",
+                id: "101"},
+                {location: "Denver, CO",
+                id: "181"},
+                {location: "Marietta, GA",
+                id: "041"},
+                {location: "Chicago, IL",
+                id: "151"},
+                {location: "Westmont, IL",
+                id: "025"},
+                {location: "Overland Park, KS",
+                id: "191"},
+                {location: "Cambridge, MA",
+                id: "121"},
+                {location: "Rockville, MD",
+                id: "085"},
+                {location: "Parkville, MD",
+                id: "125"},
+                {location: "Madison Heights, MI",
+                id: "055"},
+                {location: "St. Louis Park, MN",
+                id: "045"},
+                {location: "Brentwood, MO",
+                id: "095"},
+                {location: "Paterson, NJ",
+                id: "075"},
+                {location: "Westbury, NY",
+                id: "171"},
+                {location: "Brooklyn, NY",
+                id: "115"},
+                {location: "Flushing, NY",
+                id: "145"},
+                {location: "Yonkers, NY",
+                id: "105"},
+                {location: "Columbus, OH",
+                id: "141"},
+                {location: "Mayfield Heights, OH",
+                id: "051"},
+                {location: "Sharonville, OH",
+                id: "071"},
+                {location: "St. Davids, PA",
+                id: "061"},
+                {location: "Houston, TX",
+                id: "155"},
+                {location: "Dallas, TX",
+                id: "131"},
+                {location: "Fairfax, VA",
+                id: "081"}]
+
         }
     },
     methods: {
         //call function passing the value of card input from front end
-        search(card) {
+        search(card, selectLocation, sentReq) {
+            sentReq = [{card, selectLocation}]
             axios
             //post call to send value of card to server so it can make correct crawler
-            .post("https://gpu-scraper.herokuapp.com/stock", card)
+            .post("https://gpu-scraper.herokuapp.com/stock", sentReq)
             .then(response => {
                 //return data from server crawler if something is returned set show to true to show cards on front end
                 this.resData = response.data
@@ -27,6 +82,9 @@ const app = Vue.createApp({
                 console.log(e)
                 this.show = false
             })
+        },
+        location(loc) {
+            this.selectLocation = loc.target.value
         }
     },
 })
